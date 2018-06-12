f7c3c292b8de9ed0f3fca999c79e6f37076dc309
rohit <you@example.com>
2018-06-12 10:48:22 +0530

    first commit

---
+++ validation.php
@@ -0,0 +1,33 @@
+<?php
+
+session_start();
+header('location:login.php');
+
+$con = mysqli_connect('localhost','root');
+
+if($con){
+	echo "Connection successful";
+}else{
+	echo "No Connection";
+}
+
+mysqli_select_db($con,'sessionpractical');
+
+$name = $_POST['user'];
+$pass = $_POST['password'];
+
+$q = "select * from signin where name ='$name' && password ='$pass' ";
+
+$result = mysqli_query($con ,$q);
+
+$num = mysqli_num_rows($result);
+
+if($num==1){
+	$_SESSION['username'] = $name;
+	header('location:home.php');
+}else{
+	header('location:login.php');
+
+}
+
+?>
